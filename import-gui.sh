f9fdf2f5504595ef5ce3696cbf2b947e5f9550dd
Ahmed Waleed <ahmedwaleed@csed22.com>
2021-11-02 19:11:16 +0200

    Keep looking ahead, good GUI but dead

--- import-gui.sh
+++ import-gui.sh
@@ -1,1 +1,3 @@
+# after using this line, replace :/imgs with ../imgs
+# and delete import resources_rc
 pyuic5 -x "D:\Code\Qt\8-puzzle-ai\form.ui" -o "D:\Code\PyCharm\8-puzzle-ai\src\gui\GameGUI.py"
