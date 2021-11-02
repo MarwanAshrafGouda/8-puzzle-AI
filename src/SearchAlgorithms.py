39c2187b015d0e7606239b71c02acd83b0707a6d
Marwan Ashraf <maroashraf428@gmail.com>
2021-11-02 15:00:35 +0200

    Fixed max depth

--- src/SearchAlgorithms.py
+++ src/SearchAlgorithms.py
@@ -10,17 +10,26 @@ from Heuristics import Heuristic
 class SearchAlgorithm(ABC):
     def __init__(self):
         self.__expanded = set()  # a set of all expanded nodes
+        self.__max_depth = 0
 
     @property
     def expanded(self):
         return self.__expanded
+
+    @property
+    def max_depth(self):
+        return self.__max_depth
+
+    @max_depth.setter
+    def max_depth(self, max_depth):
+        self.__max_depth = max_depth
 
     @abstractmethod
     def search(self, initial_state: GameState):
         pass
 
 
-# a parent class to DFS and BFS
+# a parent class for DFS and BFS
 class UninformedSearch(SearchAlgorithm):
     def __init__(self):
         super().__init__()
@@ -36,21 +45,24 @@ class UninformedSearch(SearchAlgorithm):
         while self._frontier:
             curr = self.remove_from_frontier()
             self.expanded.add(curr.configuration)
+            self.max_depth = max(self.max_depth, curr.depth)
             if curr.is_goal():
-                return curr, self.expanded
+                return curr, self.expanded, self.max_depth
             for child in curr.spawn_children():
                 if child.configuration not in self.expanded and child not in self._frontier:
                     self._frontier.append(child)
-        return None, self.expanded
+        return None, self.expanded, self.max_depth
 
 
 class BFS(UninformedSearch):
     def remove_from_frontier(self):
+        # treat frontier as a queue
         return self._frontier.popleft()
 
 
 class DFS(UninformedSearch):
     def remove_from_frontier(self):
+        # treat frontier as a stack
         return self._frontier.pop()
 
 
@@ -65,10 +77,11 @@ class AStar(SearchAlgorithm):
         while frontier:
             curr = heapq.heappop(frontier)
             self.expanded.add(curr.configuration)
+            self.max_depth = max(self.max_depth, curr.depth)
             if curr.is_goal():
-                return curr, self.expanded
+                return curr, self.expanded, self.max_depth
             for child in curr.spawn_children():
                 if child.configuration not in self.expanded and child not in frontier:
                     child.heuristic = self.__heuristic.calculate_cost(child)
                     heapq.heappush(frontier, child)
-        return None, self.expanded
+        return None, self.expanded, self.max_depth
