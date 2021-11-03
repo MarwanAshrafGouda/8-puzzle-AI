5954c3e48f5510a79ff67f59e7b8acfd47382531
Ahmed Waleed <ahmedwaleed@csed22.com>
2021-11-03 22:31:03 +0200

    One button a day, keeps Maro at Bay

--- src/Game.py
+++ src/Game.py
@@ -1,34 +1,10 @@
-# the main file in which the game is played
-# should be the interface to the program, display GUI, keep track of time,
-# shuffle initial state or allow user to enter it ...etc
-import timeit
+import sys
+import GameGUI as gui
+from PyQt5 import QtWidgets
 
-from Heuristics import *
-from SearchAlgorithms import *
-
-
-def visualize_board(string: str):
-    for i in range(0, 3):
-        print(string[i * 3: i * 3 + 3])
-    print('')
-
-
-initial_state = GameState("125340678")
-algorithm = AStar(ManhattanHeuristic())
-# algorithm = AStar(EuclideanHeuristic())
-# algorithm = BFS()
-# algorithm = DFS()
-start = timeit.default_timer()
-goal, expanded = algorithm.search(initial_state)
-stop = timeit.default_timer()
-print("Runtime: " + str((stop - start) * 1000) + " ms")
-print("Number of expanded nodes: " + str(len(expanded)))
-print("Cost: " + str(goal.movement_cost))
-print("Goal depth: " + str(goal.depth) + '\n')
-sequence = []
-while goal:
-    sequence.append(goal.configuration)
-    goal = goal.parent
-for config in reversed(sequence):
-    visualize_board(config)
-
+app = QtWidgets.QApplication(sys.argv)
+GameGUI = QtWidgets.QMainWindow()
+ui = gui.Ui_GameGUI()
+ui.setupUi(GameGUI)
+GameGUI.show()
+sys.exit(app.exec_())
