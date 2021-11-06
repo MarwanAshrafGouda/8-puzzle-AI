af5a0507e9f3c47ebcbd8944a8988c8beb9629ff
Ahmed Waleed <ahmedwaleed@csed22.com>
2021-11-06 20:41:50 +0200

    Please sumbit :'(

--- src/utils.py
+++
@@ -1,8 +0,0 @@
-# turns the given string to a grid-like format
-def string_to_grid(config: str):
-    grid = ''
-    for i in range(0, 3):
-        for j in range(0, 3):
-            grid += config[3 * i + j % 3]
-        grid += '\n'
-    return grid
